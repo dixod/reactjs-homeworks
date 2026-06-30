6335bf0a5099b79e5d508752f181035ac9a949a2
dixod <184384704+dixod@users.noreply.github.com>
2026-06-30 18:27:02 +0300

    hw5 fix

--- src/api/ordersApi.js
+++ src/api/ordersApi.js
@@ -1,7 +1,7 @@
 const ORDERS = 'https://65de35f3dccfcd562f5691bb.mockapi.io/api/v1/orders';
 
-export async function getOrders() {
-  const response = await fetch(ORDERS);
+export async function getOrders(request = fetch) {
+  const response = await request(ORDERS);
 
   if (!response.ok) {
     throw new Error(`Failed to load orders (${response.status})`);
